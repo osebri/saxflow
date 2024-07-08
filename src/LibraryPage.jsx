087e2c62bcd16e11d3c39392bcd37d4f271f356c
omar sebri <osebr009@uottawa.ca>
2024-07-08 07:16:13 -0400

    try 5

--- src/LibraryPage.jsx
+++ src/LibraryPage.jsx
@@ -23,7 +23,7 @@ const LibraryPage = () => {
 
   const musicSheets = [
     {
-      path: 'hymntun.pdf',
+      path: `${process.env.PUBLIC_URL}/hymntun.pdf`,
       title: 'Sheet 1',
       author: 'Author 1',
       preview: 'sh2.jpg',
